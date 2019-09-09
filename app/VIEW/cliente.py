cd657bd20b563fbe495667ccf84e7274fccb08fd
TyphonBak <andrade.czr@gmail.com>
2019-09-09 10:03:34 -0300

    adjust url for apis

--- app/VIEW/cliente.py
+++ app/VIEW/cliente.py
@@ -3,27 +3,27 @@ from app.BLL.cliente_service import lista as lista_service, busca as busca_servi
 
 bp_cliente = Blueprint('Cliente BP', __name__)
 
-@bp_cliente.route('/clientes')
+@bp_cliente.route('/api/clientes')
 def lista():
     result = lista_service()
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_cliente.route('/clientes/<int:id>')
+@bp_cliente.route('/api/clientes/<int:id>')
 def busca(id):
     result = busca_service(id)
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_cliente.route('/clientes', methods=['POST'])
+@bp_cliente.route('/api/clientes', methods=['POST'])
 def adiciona():
     result = adiciona_service(request.get_json())
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_cliente.route('/clientes/<int:id>', methods=['PUT'])
+@bp_cliente.route('/api/clientes/<int:id>', methods=['PUT'])
 def altera(id):
     result = altera_service(id, request.get_json())
     return jsonify(result['conteudo']), result['code']
 
-@bp_cliente.route('/clientes/<int:id>', methods=['DELETE'])
+@bp_cliente.route('/api/clientes/<int:id>', methods=['DELETE'])
 def deleta(id):
     result = deleta_service(id)
     return jsonify(result['conteudo']), result['code']
--- app/VIEW/filme.py
+++ app/VIEW/filme.py
@@ -3,27 +3,27 @@ from app.BLL.filme_service import lista as lista_service, busca as busca_service
 
 bp_filme = Blueprint('Filme BP', __name__)
 
-@bp_filme.route('/filmes')
+@bp_filme.route('/api/filmes')
 def lista():
     result = lista_service()
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_filme.route('/filmes/<int:id>')
+@bp_filme.route('/api/filmes/<int:id>')
 def busca(id):
     result = busca_service(id)
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_filme.route('/filmes', methods=['POST'])
+@bp_filme.route('/api/filmes', methods=['POST'])
 def adiciona():
     result = adiciona_service(request.get_json())
     return jsonify(result.get('conteudo')), result.get('code')
 
-@bp_filme.route('/filmes/<int:id>', methods=['PUT'])
+@bp_filme.route('/api/filmes/<int:id>', methods=['PUT'])
 def altera(id):
     result = altera_service(id, request.get_json())
     return jsonify(result['conteudo']), result['code']
 
-@bp_filme.route('/filmes/<int:id>', methods=['DELETE'])
+@bp_filme.route('/api/filmes/<int:id>', methods=['DELETE'])
 def deleta(id):
     result = deleta_service(id)
     return jsonify(result['conteudo']), result['code']
